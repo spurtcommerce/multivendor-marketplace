10951bb73670efd640aabad2fdabadf75208c6ed
Picco Server <piccoserver@piccsoft.com>
2023-03-25 22:54:37 +0530

    New 4.8.1 admin dist added

--- admin-angular/src/environments/environment.dev.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  baseUrl: '<Your API base url>',
-  imageUrl: '<Your API url for image resize>',
-  productUrl: '<Your store base url>',
-  pluginUrl: '<Your API url>'
-};
--- admin-angular/src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  baseUrl: 'http://3.6.249.0/backend/api',
-  imageUrl: 'https://image.spurtcart.com/',
-  productUrl: 'http://3.6.249.0/',
-  pluginUrl: 'http://3.6.249.0/backend/'
-
-};
